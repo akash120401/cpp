661c21b714cc9976dc19581b6f50a70a76819c48
akash120401 <166601244+akash120401@users.noreply.github.com>
2024-04-10 14:30:08 +0530

    Add files via upload

---
+++ practices.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+class car {
+	private : int price=1100;
+	
+	         int id=120;
+	         public:
+	         int getpr(){
+	         	return price;
+	         	
+			 }
+	         int getId(){
+	         	return id;
+	         	
+			 }
+	         
+	         void setPrice(int ak){
+	         	price=ak;
+	         	}
+	
+	
+	
+	
+	
+};
+
+int main (){
+	car obj;
+	
+	obj.setPrice(500);
+	cout<<"....getnd...."<<obj.getpr()<<obj.getId()<<endl;
+    //cout<<"....getnd...."<<obj.setPrice(500)<<endl; 
+
+}
